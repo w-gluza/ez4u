45720b399e39d626c2e9790ac94b32719f694921
Wioletta Gluza <wiolagluza@gmail.com>
2018-09-19 16:08:30 +0100

    prod config updated

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -218,7 +218,7 @@ module.exports = {
             )
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
-          // "file" loader makes sure assets end up in the `build` folder.
+          // "file" loader makes sure assets end up in the `build` folder
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
           // that fall through the other loaders.
